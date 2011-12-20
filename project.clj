4f140433f7900590d538308d7b37f68401680133
Eugene Koontz <ekoontz@hiro-tan.org>
2011-12-20 12:30:07 -0800

    add description and deps

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
+;; TODO: not tested or used yet :using Makefile for building and repl for now.
 (defproject cia-ch-5 "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :description "experiments based on Chapter 5 of Clojure In Action by Amit Rathore."
+  :dependencies [[org.clojure/clojure "1.4.0-SNAPSHOT"]])
