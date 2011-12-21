8951a39c078fe60c81c6831ab5b0e8354730b37f
Eugene Koontz <ekoontz@hiro-tan.org>
2011-12-20 18:20:04 -0800

    add comment

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-;; TODO: not tested or used yet :using Makefile for building and repl for now.
+;; TODO: move functionality out of Makefile and into here as I gradually learn leiningen.
 (defproject cia-ch-5 "1.0.0-SNAPSHOT"
   :description "experiments based on Chapter 5 of Clojure In Action by Amit Rathore."
   :dependencies [[org.clojure/clojure "1.3.0"]])
